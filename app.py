a72dce38039b5b6d28fefc22ecbc1b271e634a00
Reslan AlTinawi <reslan3121@gmail.com>
2022-08-07 20:35:58 +0300

    Working app
    - Update requirements file. Add `black` package.
    
    - Rename `streamlit_app` to `app` and finalize app logic.
    
    - Add new data file `syria_crop_data_cleaned` which contains ready-to-plot data.

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import plotly.express as px
+
+import pandas as pd
+
+crop_data = pd.read_parquet("data/syria_crop_data_cleaned.parquet")
+
+crop_production_df = crop_data.loc[crop_data["Element"] == "Production"]
+
+crop_production_stats = (
+    crop_production_df.groupby(by="Item", as_index=False)
+    .aggregate(n_years=pd.NamedAgg(column="Year", aggfunc="nunique"))
+    .sort_values(by="n_years", ascending=False)
+)
+
+crop_items_to_ignore = crop_production_stats.loc[crop_production_stats["n_years"] != 60, "Item"].values
+
+crop_production_df = crop_production_df.loc[~crop_production_df["Item"].isin(crop_items_to_ignore)]
+
+total_production_by_item = (
+    crop_production_df.loc[crop_production_df["Unit"] == "tonnes"]
+    .groupby(by="Item", as_index=False)
+    .aggregate(total_production=pd.NamedAgg(column="Value", aggfunc="sum"))
+    .sort_values(by="total_production", ascending=False)
+)
+
+fig = px.line(
+    data_frame=crop_production_df.loc[crop_production_df["Item"].isin(total_production_by_item.iloc[:15, 0])],
+    x="Year",
+    y="Value",
+    color="Item",
+    title="Yearly crop production data (measured in tonnes)",
+    labels={"Item": "Crop"},
+    height=600,
+)
+
+# fig.show()
+st.set_page_config(layout="wide")
+st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
