15dae91f2dba61bab1eae9a64f9b7ddcb7d70957
reslan <reslan3121@gmail.com>
2023-02-08 14:58:03 +0300

    NDVI Choropleth map
    - add NDVI choropleth figure using Plotly

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+import plotly
 
 import pandas as pd
 
@@ -13,9 +14,13 @@ crop_production_stats = (
     .sort_values(by="n_years", ascending=False)
 )
 
-crop_items_to_ignore = crop_production_stats.loc[crop_production_stats["n_years"] != 60, "Item"].values
+crop_items_to_ignore = crop_production_stats.loc[
+    crop_production_stats["n_years"] != 60, "Item"
+].values
 
-crop_production_df = crop_production_df.loc[~crop_production_df["Item"].isin(crop_items_to_ignore)]
+crop_production_df = crop_production_df.loc[
+    ~crop_production_df["Item"].isin(crop_items_to_ignore)
+]
 
 total_production_by_item = (
     crop_production_df.loc[crop_production_df["Unit"] == "tonnes"]
@@ -25,7 +30,9 @@ total_production_by_item = (
 )
 
 fig = px.line(
-    data_frame=crop_production_df.loc[crop_production_df["Item"].isin(total_production_by_item.iloc[:15, 0])],
+    data_frame=crop_production_df.loc[
+        crop_production_df["Item"].isin(total_production_by_item.iloc[:5, 0])
+    ],
     x="Year",
     y="Value",
     color="Item",
@@ -34,6 +41,16 @@ fig = px.line(
     height=600,
 )
 
-# fig.show()
 st.set_page_config(layout="wide")
 st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
+
+ndvi_mapbox_choropleth_fig = plotly.io.read_json(
+    "figures/ndvi-mapbox-choropleth-map.json"
+)
+
+st.plotly_chart(
+    ndvi_mapbox_choropleth_fig,
+    use_container_width=True,
+    sharing="streamlit",
+    config={"scrollZoom": False, "displayModeBar": False},
+)
